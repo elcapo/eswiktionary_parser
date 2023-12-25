7acedfea35e144c5df7984dfecf961378247990b
Carlos Capote Pérez-Andreu <carlos.capote@hawara.es>
2023-12-25 15:13:41 +0000

    simplify wiki parser tests

--- eswiktionary_parser/fixture_reader.py
+++ eswiktionary_parser/fixture_reader.py
@@ -1,4 +1,10 @@
+from eswiktionary_parser.wiki_parser import parse_and_get_definitions
+
 def read_fixture(filename: str) -> str:
     with open(filename, "r") as file:
         content = file.read()
     return content
+
+def read_and_parse_fixture(filename: str) -> list:
+    content = read_fixture(filename)
+    return parse_and_get_definitions(content)
--- tests/test_wiki_parser.py
+++ tests/test_wiki_parser.py
@@ -1,26 +1,21 @@
-from eswiktionary_parser.fixture_reader import read_fixture
-from eswiktionary_parser.wiki_parser import parse_and_get_definitions
+from eswiktionary_parser.fixture_reader import read_and_parse_fixture
 
 def test_can_extract_definitions():
-    wikitext = read_fixture("tests/fixtures/palabra.wiki")
-    definitions = parse_and_get_definitions(wikitext)
+    definitions = read_and_parse_fixture("tests/fixtures/palabra.wiki")
     assert len(definitions) == 7
     assert definitions[0].definition == "Unidad mínima de significado de una lengua o idioma."
 
 def test_ignores_examples():
-    wikitext = read_fixture("tests/fixtures/prótesis.wiki")
-    definitions = parse_and_get_definitions(wikitext)
+    definitions = read_and_parse_fixture("tests/fixtures/prótesis.wiki")
     assert len(definitions) == 3
     assert definitions[1].definition == "Dispositivo que sirve para sustituir el miembro, órgano o parte del órgano que ha experimentado algún tipo de daño o pérdida."
 
 def test_removes_unwanted_prefixes():
-    wikitext = read_fixture("tests/fixtures/elemental.wiki")
-    definitions = parse_and_get_definitions(wikitext)
     # Remove the prefix "Fig."
+    definitions = read_and_parse_fixture("tests/fixtures/elemental.wiki")
     assert definitions[1].definition == "Primordial, fundamental."
 
 def test_ignores_synonyms():
-    wikitext = read_fixture("tests/fixtures/uno.wiki")
-    definitions = parse_and_get_definitions(wikitext)
+    definitions = read_and_parse_fixture("tests/fixtures/uno.wiki")
     assert definitions[6].definition == "Que no posee igual."
     assert definitions[7].definition == "Que se identifica con otra persona o cosa en un plano espiritual, moral o que se siente físicamente unido."
